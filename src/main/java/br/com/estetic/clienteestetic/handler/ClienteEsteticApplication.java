4bd554203fd47159ea422a8d2d71d945bb36f2f9
thimooura <t.695@hotmail.com>
2023-03-29 15:05:52 -0300

    inicia o projeto

---
+++ src/main/java/br/com/estetic/clienteestetic/handler/ClienteEsteticApplication.java
@@ -0,0 +1,23 @@
+package br.com.estetic.clienteestetic.handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping
+public class ClienteEsteticApplication {
+
+
+	@GetMapping
+	public String getHomeTeste() {
+		return "Cliente Pet - API Home";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientePetApplication.class, args);
+	}
+}
